8346a69be9ea17f2a6cdee9fe0f6ff81f2af5602
hkp <sxhkp163@163.com>
2020-04-13 15:55:07 +0800

    合并

--- README.md
+++
@@ -1,2 +0,0 @@
-# TypeScript
-typescript lesson demo
--- src/gameComp/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import GameComp from "../component/gameComp";
-
-function GameCompWrap(){
-    return(
-        <GameComp />
-    )
-};
-export default GameCompWrap;
